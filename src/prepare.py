689e5215a27e0f6f0e8b3030958acbc76f8f7a23
polmonroig <polmonroig@yahoo.es>
2020-06-17 12:33:47 +0200

    Modify saving structure

--- src/prepare.py
+++ src/prepare.py
@@ -1,17 +1,11 @@
-import utils 
+import utils
 
 
 def main():
     print('Preparing data...')
-    data = utils.Data() 
     transforms = []
-    print('Reading data...')
-    data.read_and_prepare(transforms)
-    # saves data as train/test .csv in data directory 
-    print('Saving data...')
-    data.write() 
-
-
+    data = utils.Data(transforms=transforms)
+    data.prepare_and_write()
 
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -13,92 +13,74 @@ data_links_small = os.path.join(data_dir, 'links_small.csv')
 data_movies_metadata = os.path.join(data_dir, 'movies_metadata.csv')
 data_ratings = os.path.join(data_dir, 'ratings.csv')
 data_ratings_small = os.path.join(data_dir, 'ratings_small.csv')
-data_train = os.path.join(data_dir, 'train.csv')
-data_test = os.path.join(data_dir, 'test.csv')
+data_train = os.path.join(data_dir, 'train/data')
+data_test = os.path.join(data_dir, 'test/data')
 
 # global model definitions
 model_dir = '../models'
 
 
-class Data:
+class DataProcessor:
     """
     The dataset encapsulates all the data set and it is in charge
     of preforming any feature extraction, filtering and parsing
     needed for the training. It also separates the data into a
-    train and test set that can be easily called. Since the data
-    is small enough, it can be read completely before training,
-    it does not involve any parallel read/train architecture
-    such as with torch.utils.data.
+    train and test set that can be easily called. The complete dataset
+    is not too big, but since we need to generate a sparse matrix with it
+    the size grows inmensely. To solve this issue we separate the data into
+    multiple files that must be loaded concurrently while training.
 
     Attributes
     self.test_split (float): the percentage of data that belongs to the test set
-    self.train (np.array): contains the training set
-    self.test (np.array): contains the test/eval set
+    self.seed (int): seed used to split the dataset
+    self.users_per_file (int): max users per file of data
+    self.transforms (array): list of transformations to apply to the data
     """
-    def __init__(self, test_split=0.2, seed=42):
+    def __init__(self, test_split=0.2, seed=42, users_per_file=128, transforms):
         self.test_split = test_split
-        self.train = None
-        self.test = None
         self.seed = seed
+        self.users_per_file = 128
+        self.transforms = transforms
 
-    def read(self):
-        """
-        Reads the already prepared data from disk,
-        call this function when training the network
-        """
-        self.train = pd.read_csv(data_train)
-        self.test = pd.read_csv(data_test)
-
-    def read_and_prepare(self, transforms):
+    def prepare_and_write(self):
         """
         Reads the data from disks and performs
         all the data wrangling required
         :param transforms list of transformations to apply
         :return: None
         """
-        features, target = Data.read_unprocessed()
-        # apply transforms
-        for t in transforms:
-            features = t(features)
         # split dataset
-        result = train_test_split(features, target,
-                                  test_size=self.test_split, random_state=self.seed)
+        data = pd.read_csv(data_ratings)
+        # split dataset
+        train, test = train_test_split(data, test_size=self.test_split, random_state=self.seed)
+        self.save_data(data_train, train)
+        self.save_data(data_test, test)
 
-        self.train = (result[0], result[2])
-        self.test = (result[1], result[3])
 
-    @staticmethod
-    def read_unprocessed():
-        """
-        Read features and target for processing, we are working in
-        an unsupervised task with collaborative filtering so
-        the features must be equal to the target
-        :return:
-        """
-        data = pd.read_csv(data_ratings_small)
+
+    def save_data(self, name, data):
         users = data['userId']
         movies = data['movieId']
         ratings = data['rating']
         total_movies = len(set(movies.tolist()))
-        total_users = len(set(users.tolist()))
-        print('Features size:', (total_users, total_movies))
-        features = np.zeros((total_users, total_movies), dtype=np.float32)
-        for user, rating in zip(users, ratings):
-            features[user - 1] = rating
-        target = features
+        total_files = total_users // users_per_file
 
-        return features, target
+        for file in range(total_files):
+            total_users = total_files * file
+            shape = (total_users, total_movies)
+            dataset = np.zeros((total_users, total_movies), dtype=np.float32)
 
-    def write(self):
-        """
-        Saves the processed train  and test set to the data directory
-        :return: None
-        """
-        np.savetxt(data_train, self.train[0], delimiter=',')
-        np.savetxt(data_test, self.test[1], delimiter=',')
+            for user, rating in zip(users, ratings):
+                features[user - 1] = rating
+            file_name = name + '_' + DataProcessor.padding(file, total_files) + '.csv'
+            print('Saving file', file_name)
+            np.savetxt(file_name, dataset, delimiter=',')
 
-    def train(self):
-        return self.train
+    @staticmethod
+    def padding(number, size):
+        file = str(file)
+        size = str(total_files)
+        while len(file) < len(size):
+            file = '0' + file
 
-    def test(self):
-        return self.test
+        return file
