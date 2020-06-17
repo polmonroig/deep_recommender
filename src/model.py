44701be8e5c4805c1b302c5bffe423f6973b51b0
polmonroig <polmonroig@yahoo.es>
2020-06-17 13:04:05 +0200

    Add autoencoder template

--- src/model.py
+++ src/model.py
@@ -1,3 +1,17 @@
+import torch.nn as nn
 
 
+class AutoEncoder(nn.module):
 
+    def __init__(self):
+        super().__init__()
+
+
+    def encode(self, x):
+        return x
+
+    def decode(self, x):
+        return x
+
+    def forward(self, x):
+        return seld.decode(self.encode(x))
