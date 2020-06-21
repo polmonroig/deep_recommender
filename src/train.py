d62dd94158819f8f37834d5b0b1dddef2ececd87
polmonroig <polmonroig@yahoo.es>
2020-06-21 21:13:02 +0200

    Add more things to the training loop

--- src/train.py
+++ src/train.py
@@ -1,7 +1,9 @@
 from utils import RatingsDataset
 from model import BasicAutoencoder
 from argparse import ArgumentParser
+from torch.utils.data import DataLoader
 import torch
+import torch.optim as optim
 
 
 def get_parser():
@@ -13,14 +15,15 @@ def get_parser():
     parser.add_argument('--n_epochs', type=int, help='Number of epochs for the training loop')
     parser.add_argument('--batch_size', type=int, help='Batch size of the training and eval set')
     parser.add_argument('--verbose_epochs', type=int, help='Number of epochs per training verbose output')
+    parser.add_argument('--lr', type=float, help='Learning rate of the optimizer')
     return parser
 
 
-def eval_step(model):
+def eval_step(model, x, y):
     raise NotImplementedError('Eval step currently not implemented')
 
 
-def train_step(model):
+def train_step(model, optimizer, x, y):
     raise NotImplementedError('Train step currently not implemented')
 
 
@@ -33,6 +36,7 @@ def main():
     n_epochs = args.n_epochs
     batch_size = args.batch_size
     verbosity = args.verbose_epochs
+    learning_rate = args.lr
 
     # device initialization
     device = None
@@ -44,13 +48,25 @@ def main():
         print('Running on cpu')
     # dataset creation
     dataset = RatingsDataset(add_noise)
-    model = BasicAutoencoder()
+    dataloader = DataLoader(dataset, batch_size,shuffle=False, num_workers=4)
+    model = BasicAutoencoder().to(device)
+    optimizer = optim.Adam(model.params(), lr=learning_rate)
+
     # train loop
     for epoch in range(n_epochs):
         if epoch % verbosity == 0:
             print('Epoch', epoch, '/', n_epochs)
 
-        error = 0
+        for batch in dataloader:
+            optimizer.zero_grad()
+            x, y = batch
+            x = x.to(device)
+            y = y.to(device)
+            model.train()
+            train_step(model, optimizer, x, y)
+            model.eval()
+            eval_step(model, x, y)
+            error = 0
         if epoch % verbosity == 0:
             print('Error', error)
 
